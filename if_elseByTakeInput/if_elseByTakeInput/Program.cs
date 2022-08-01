d756d0c3c62b693c7f05aeb7f0fa2169c8e7bd20
janvi desai <janvi@webtheuniverse.com>
2022-08-01 13:33:27 +0530

    input taken by user program is done

---
+++ if_elseByTakeInput/if_elseByTakeInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_elseByTakeInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("It is even number");
+            }
+            else
+            {
+                Console.WriteLine("It is odd number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
